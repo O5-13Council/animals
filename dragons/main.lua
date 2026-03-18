e194efa1c5ccb8c1eb8a0f6b782b85ad5f32e4cf
O5-13Council <118745520+O5-13Council@users.noreply.github.com>
2026-03-17 22:59:48 -0400

    Add files via upload

---
+++ dragons/main.lua
@@ -0,0 +1,30 @@
+--thanks to diriel, blert2112 and taikedz
+
+local tamed_dragons = {}
+
+--items and tools
+
+core.register_craftitem("dmobs:dragon_gem_lightning", {
+	description = "Lightning Gem",
+	inventory_image = "dmobs_gem_lightning.png"
+})
+
+core.register_craftitem("dmobs:dragon_gem_ice", {
+	description = "Ice Gem",
+	inventory_image = "dmobs_gem_ice.png"
+})
+
+core.register_craftitem("dmobs:dragon_gem_fire", {
+	description = "Fire Gem",
+	inventory_image = "dmobs_gem_fire.png"
+})
+
+core.register_craftitem("dmobs:dragon_gem_poison", {
+	description = "Poison Gem",
+	inventory_image = "dmobs_gem_poison.png"
+})
+
+core.register_craftitem("dmobs:dragon_gem", {
+	description = "Dragon Gem",
+	inventory_image = "dmobs_gem.png"
+})
